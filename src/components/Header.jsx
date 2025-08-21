c165a527bde031d775578e1daed61ace1f8469d7
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-08-21 13:35:26 +0900

    tailwindcss로 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
-    <header className="header-container">
-      <h2>OZ코딩스쿨</h2>
-      <ul className="menu-container">
+    <header className="flex justify-between items-center px-[20px] py-[15px] bg-black">
+      <h2 className="text-[18px] font-bold">OZ코딩스쿨</h2>
+      <ul className="flex justify-center items-center gap-[20px] list-none font-semibold text-[13px]">
         <li>로그인</li>
         <li>회원가입</li>
         <li>내클래스</li>
